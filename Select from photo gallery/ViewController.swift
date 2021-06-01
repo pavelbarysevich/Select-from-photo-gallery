4bcf5b5d72bc872ef0edf60dcf4bd57d8ed8db2b
Pavel Barysevich <pavelbarysevich@gmail.com>
2021-06-01 16:46:53 +0300

    Initial Commit

---
+++ Select from photo gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Select from photo gallery
+//
+//  Created by Павел Борисевич on 1.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
